5c3b3beecb3b10aa17b9f6ed355badb676464107
Suyash Potdar <84835929+Codder-lab@users.noreply.github.com>
2024-07-16 17:26:04 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,3 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    const toggleSwitch = document.querySelector('#dark-mode-toggle');
-    const body = document.body;
-
-    toggleSwitch.addEventListener('change', () => {
-        if (toggleSwitch.checked) {
-            body.classList.add('dark-mode');
-        } else {
-            body.classList.remove('dark-mode');
-        }
-    });
-});
