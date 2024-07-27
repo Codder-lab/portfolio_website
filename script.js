d2185ba2838ffe53e4fdc794b3c352e313673279
Suyash Potdar <84835929+Codder-lab@users.noreply.github.com>
2024-07-27 09:58:20 +0000

    Update animations and Blog

--- script.js
+++ script.js
@@ -3,4 +3,31 @@ function toggleMenu() {
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
+    menu.classList.toggle("show");
 }
+
+// script.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    const elements = document.querySelectorAll('.scroll-animation');
+    const profilePic = document.querySelector('.profile-pic-animation');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animated');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    elements.forEach(element => {
+        observer.observe(element);
+    });
+
+    if (profilePic) {
+        observer.observe(profilePic);
+    }
+});
